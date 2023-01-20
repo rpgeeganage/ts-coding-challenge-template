381aa58e3d05a3409317daf2c02d847437d07e29
Ruwan Geeganage <ruwangeeganage@Ruwans-MacBook-Pro-2.local>
2023-01-20 16:39:03 +0100

    sample config file

---
+++ src/configs.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+const envSchema = z.object({
+  PORT: z.number()
+});
+
+const parsedConfig = envSchema.safeParse(process.env);
+if (!parsedConfig.success) {
+  throw new Error(
+    `Configuration failuer. ${JSON.stringify(parsedConfig.error.format())}`
+  );
+}
+
+export const Configs = parsedConfig.data;
+export type ConfigsType = z.input<typeof envSchema>;
+
