19dbb41dc23cdc5c395b1c4b1dcc56c53d7d4db1
Ruwan Geeganage <ruwangeeganage@Ruwans-MacBook-Pro-2.local>
2023-01-20 18:42:47 +0100

    settup prettier and lint

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import express from 'express';
 import { Configs } from 'src/configs';
 
 const app = express();
-
 app.listen(Configs.PORT, () => {
   console.log(`Server started at port: ${Configs.PORT}`);
 });
