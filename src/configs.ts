6fffbe397e52839a3bb98d06ca65ad4e63112146
Ruwan Geeganage <ruwangeeganage@Ruwans-MacBook-Pro-2.local>
2023-01-21 20:15:20 +0100

    check husky

--- src/configs.ts
+++ src/configs.ts
@@ -6,7 +6,7 @@ const envSchema = z.object({
 
 const parsedConfig = envSchema.safeParse(process.env);
 if (!parsedConfig.success) {
-  throw new Error(`Configuration failuer. ${JSON.stringify(parsedConfig.error.format())}`);
+  throw new Error( `Configuration failuer. ${JSON.stringify(parsedConfig.error.format())}`);
 }
 
 export const Configs = parsedConfig.data;
